0ff1f176d937575ed568bf5d8b08de3e15629686
CornelI5 <kenzoyooo0@gmail.com>
2026-07-19 08:51:33 +0700

    Create compiler.h

---
+++ Include/compiler.h
@@ -0,0 +1,7 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+// Triggers the entire compilation pipeline from raw code to executable
+void compile_file(const char *source_code, const char *output_binary);
+
+#endif
