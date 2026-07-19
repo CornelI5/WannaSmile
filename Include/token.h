f3c5dab367a3c5b24dd984be2890aa3a79ef7cdc
CornelI5 <kenzoyooo0@gmail.com>
2026-07-19 08:10:10 +0700

    Update token.h

--- Include/token.h
+++ Include/token.h
@@ -4,12 +4,12 @@
 typedef enum {
     TOK_INC, TOK_TYPE, TOK_MAIN, TOK_LPAREN, TOK_RPAREN,
     TOK_LBRACE, TOK_RBRACE, TOK_SEMICOLON, TOK_IDENT,
-    TOK_STRING, TOK_PRINT, TOK_RETURN, TOK_EOF
+    TOK_STRING, TOK_PRINT, TOK_RETURN, TOK_EOF, TOK_ERROR
 } TokenType;
 
 typedef struct {
     TokenType type;
-    char value[64];
+    char *value;
 } Token;
 
 #endif
